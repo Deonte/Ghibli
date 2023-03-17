39c268f9475c158e8de68dd7970975f96038909b
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2023-03-17 14:45:56 -0400

    Initial Commit

---
+++ Ghibli/GhibliApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliApp.swift
+//  Ghibli
+//
+//  Created by Deonte Kilgore on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct GhibliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
