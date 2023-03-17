d5b932f1c3da13b8b586b0808cb9273329b1a898
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2023-03-17 14:55:35 -0400

    Added app icon, and local data.

---
+++ Ghibli/Views/MovieCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCell.swift
+//  Ghibli
+//
+//  Created by Deonte Kilgore on 3/17/23.
+//
+
+import SwiftUI
+
+struct MovieCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieCell_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieCell()
+    }
+}
